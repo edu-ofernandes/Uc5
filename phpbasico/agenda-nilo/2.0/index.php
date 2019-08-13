32699c0b0b5586e09519f8f5212436683f3e9b37
edu-ofernandes <eduardo123fernandes2013@hotmail.com>
2019-08-13 17:35:44 -0300

    ajustes sem sucesso na agenda 2.0 do Nilo.

--- phpbasico/agenda-nilo/2.0/index.php
+++ phpbasico/agenda-nilo/2.0/index.php
@@ -21,6 +21,8 @@ if(isset($_GET['btSerach'])){
     $letras = $_GET['txtBusca'];
     $sqlBusca = "SELECT * FROM contatos WHERE nome LIKE '". $letras."%'";
     $resultadoBusca = $banco->query($sqlBusca);
+
+    
 }
 ?>
 
@@ -42,155 +44,160 @@ if(isset($_GET['btSerach'])){
     <?php include('include/inc_menu.php'); ?>
     <main>
 
-            <section class="todos-contatos">
-                
-                <!-- loop 1 começa -->
-                <?php if(empty($_GET['id'])){  ?>
+        <section class="todos-contatos alfa-contatos alfa-lista">
+            <ul class="alfabeto alfabeto-lista">
+                <li><a href="index.php?id=A">A</a></li>
+                <li><a href="index.php?id=B">B</a></li>
+                <li><a href="index.php?id=C">C</a></li>
+                <li><a href="index.php?id=D">D</a></li>
+                <li><a href="index.php?id=E">E</a></li>
+                <li><a href="index.php?id=F">F</a></li>
+                <li><a href="index.php?id=G">G</a></li>
+                <li><a href="index.php?id=H">H</a></li>
+                <li><a href="index.php?id=I">I</a></li>
+                <li><a href="index.php?id=J">J</a></li>
+                <li><a href="index.php?id=K">K</a></li>
+                <li><a href="index.php?id=L">L</a></li>
+                <li><a href="index.php?id=M">M</a></li>
+                <li><a href="index.php?id=N">N</a></li>
+                <li><a href="index.php?id=O">O</a></li>
+                <li><a href="index.php?id=P">P</a></li>
+                <li><a href="index.php?id=Q">Q</a></li>
+                <li><a href="index.php?id=R">R</a></li>
+                <li><a href="index.php?id=S">S</a></li>
+                <li><a href="index.php?id=T">T</a></li>
+                <li><a href="index.php?id=U">U</a></li>
+                <li><a href="index.php?id=V">V</a></li>
+                <li><a href="index.php?id=W">W</a></li>
+                <li><a href="index.php?id=X">X</a></li>
+                <li><a href="index.php?id=Y">Y</a></li>
+                <li><a href="index.php?id=Z">Z</a></li>
+                <li><a href="index.php">Favoritos</a></li>
+            </ul>
+        </section>
 
-                <div class="container titulo">
-                    <h2>Lista de Contatos</h2>
-                    <div class="add-contatos"><a href="contatoAdd.php"><i class="fas fa-user-plus"></i></a></div>
-                </div>
-                <div class="container tabela">
-                    <div class="nome-tabela"><p>Nome</p></div>
-                    <div class="tel-tabela"><p>Telefone</p></div>
-                    <div class="email-tabela"><p>E-mail</p></div>
-                    <div class="vazio"></div>
-                </div>
 
-                <?php while ($row = mysqli_fetch_array($resultado)) {?>
-                <div class="container registros">
-                    <div class="nomes-registros"><p><?php echo ($row['nome']); ?></p></div>
-                    <div class="tel-registros"><p> <?php echo ($row['tel']); ?></p></div>
-                    <div class="email-registros"><p><?php echo ($row['email']); ?></p></div>
-                    <div class="botoes">
-                        <a href="contatoUp.php?id=<?php echo ($row['idcontatos']) ?>"><i class="fas fa-user-edit"></i></a>
+        <section class="todos-contatos">
 
-                        <?php if($row['favoritos'] != 0 ){?>
-                        <a href="favoritar.php?id=<?php echo ($row['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo "favorito-on";?>"></i></a>
-                        <?php }else{ ?>
-                        <a href="favoritar.php?id=<?php echo ($row['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo '';?>"></i></a>
-                        <?php }?>
-                        <a href="contatoDel.php?id=<?php echo $row['idcontatos']; ?>"><i class="fas fa-trash"></i></a>
-                    </div>
-                </div>
+        <?php if(empty($_GET['id'])){  ?>
+
+           
+            <!-- loop 1 começa -->
             
-                <?php } ?>
-            <!-- loop termina -->
-            </section>
-                <!-- loop 2 começa -->
-                <?php }else{ ?>
 
-                <?php if(mysqli_num_rows($resultadoBuscaAlfa) != 0){  ?>
+            <div class="container titulo">
+                <h2>Lista de Contatos</h2>
+                <div class="add-contatos"><a href="contatoAdd.php"><i class="fas fa-user-plus"></i></a></div>
+            </div>
+            <div class="container tabela">
+                <div class="nome-tabela"><p>Nome</p></div>
+                <div class="tel-tabela"><p>Telefone</p></div>
+                <div class="email-tabela"><p>E-mail</p></div>
+                <div class="vazio"></div>
+            </div>
 
-            <section class="todos-contatos lista-busca">
-                <div class="container titulo titulo-busca">
-                    <h2>Resultado da Pesquisa</h2>
+            <?php while ($row = mysqli_fetch_array($resultado)) {?>
+            <div class="container registros">
+                <div class="nomes-registros"><p><?php echo ($row['nome']); ?></p></div>
+                <div class="tel-registros"><p> <?php echo ($row['tel']); ?></p></div>
+                <div class="email-registros"><p><?php echo ($row['email']); ?></p></div>
+                <div class="botoes">
+                    <a href="contatoUp.php?id=<?php echo ($row['idcontatos']) ?>"><i class="fas fa-user-edit"></i></a>
+
+                    <?php if($row['favoritos'] != 0 ){?>
+                    <a href="favoritar.php?id=<?php echo ($row['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo "favorito-on";?>"></i></a>
+                    <?php }else{ ?>
+                    <a href="favoritar.php?id=<?php echo ($row['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo '';?>"></i></a>
+                    <?php }?>
+                    <a href="contatoDel.php?id=<?php echo $row['idcontatos']; ?>"><i class="fas fa-trash"></i></a>
                 </div>
+            </div>
+            <?php } ?>
+
+        <!-- loop termina -->
+        </section>
+            <!-- loop 2 começa -->
+            <?php }elseif( $_GET['id'] != "" && mysqli_num_rows($resultadoBuscaAlfa) != 0){  ?>
+
+        
+
+        <section class="todos-contatos lista-busca">
+            <div class="container titulo titulo-busca">
+                <h2>Resultado da Pesquisa</h2>
+            </div>
 
 
-                <div class="container tabela">
-                    <div class="nome-tabela"><p>Nome</p></div>
-                    <div class="tel-tabela"><p>Telefone</p></div>
-                    <div class="email-tabela"><p>E-mail</p></div>
-                    <div class="vazio"></div>
-                </div>
+            <div class="container tabela">
+                <div class="nome-tabela"><p>Nome</p></div>
+                <div class="tel-tabela"><p>Telefone</p></div>
+                <div class="email-tabela"><p>E-mail</p></div>
+                <div class="vazio"></div>
+            </div>
 
-                <?php while($row3 = mysqli_fetch_array($resultadoBuscaAlfa)){ ?>
-                
+            <?php while($row3 = mysqli_fetch_array($resultadoBuscaAlfa)){ ?>   
+            <div class="container registros">
+                <div class="nomes-registros"><p><?php echo ($row3['nome']); ?></p></div>
+                <div class="tel-registros"><p> <?php echo ($row3['tel']); ?></p></div>
+                <div class="email-registros"><p><?php echo ($row3['email']); ?></p></div>
+                <div class="botoes">
+                    <a href="contatoUp.php?id=<?php echo ($row3['idcontatos']) ?>"><i class="fas fa-user-edit"></i></a>
                     
-                <div class="container registros">
-                    <div class="nomes-registros"><p><?php echo ($row3['nome']); ?></p></div>
-                    <div class="tel-registros"><p> <?php echo ($row3['tel']); ?></p></div>
-                    <div class="email-registros"><p><?php echo ($row3['email']); ?></p></div>
-                    <div class="botoes">
-                        <a href="contatoUp.php?id=<?php echo ($row3['idcontatos']) ?>"><i class="fas fa-user-edit"></i></a>
-                        
-                        <?php if($row3['favoritos'] != 0 ){?>
-                        <a href="favoritar.php?id=<?php echo ($row3['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo "favorito-on";?>"></i></a>
-                        <?php }else{ ?>
-                        <a href="favoritar.php?id=<?php echo ($row3['idcontatos']) ?>" class=""><i class="fas fa-star"></i></a>
-                        <?php }?>
+                    <?php if($row3['favoritos'] != 0 ){?>
+                    <a href="favoritar.php?id=<?php echo ($row3['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo "favorito-on";?>"></i></a>
+                    <?php }else{ ?>
+                    <a href="favoritar.php?id=<?php echo ($row3['idcontatos']) ?>" class=""><i class="fas fa-star"></i></a>
+                    <?php }?>
 
-                        <a href="contatoDel.php?id=<?php echo $row3['idcontatos']; ?>"><i class="fas fa-trash"></i></a>
-                    </div>
+                    <a href="contatoDel.php?id=<?php echo $row3['idcontatos']; ?>"><i class="fas fa-trash"></i></a>
                 </div>
-                <?php  }?>
+            </div>
+            <?php  }?>
+       
+            <?php } elseif(mysqli_num_rows($resultadoBuscaAlfa) == 0) {    ?> 
+                <h2>Nenhum resultado</h2>
             </section>
-                <?php } else {  ?> 
-                    <h2>Nenhum resultado</h2>
-                <?php }} ?>
-                
+            <?php }?>
+
+            <?php  if(isset($_GET['btSerach']) && mysqli_num_rows($resultadoBusca) != 0){  ?>
+            
                 <!-- loop termina -->
 
+                <!-- loop 3 começa -->
+        <section class="todos-contatos lista-busca">
+            <div class="container titulo titulo-busca">
+                <h2>Resultado da Pesquisa</h2>
+            </div>
 
-                <section class="todos-contatos alfa-contatos alfa-lista">
-                <ul class="alfabeto alfabeto-lista">
-                    <li><a href="index.php?id=A">A</a></li>
-                    <li><a href="index.php?id=B">B</a></li>
-                    <li><a href="index.php?id=C">C</a></li>
-                    <li><a href="index.php?id=D">D</a></li>
-                    <li><a href="index.php?id=E">E</a></li>
-                    <li><a href="index.php?id=F">F</a></li>
-                    <li><a href="index.php?id=G">G</a></li>
-                    <li><a href="index.php?id=H">H</a></li>
-                    <li><a href="index.php?id=I">I</a></li>
-                    <li><a href="index.php?id=J">J</a></li>
-                    <li><a href="index.php?id=K">K</a></li>
-                    <li><a href="index.php?id=L">L</a></li>
-                    <li><a href="index.php?id=M">M</a></li>
-                    <li><a href="index.php?id=N">N</a></li>
-                    <li><a href="index.php?id=O">O</a></li>
-                    <li><a href="index.php?id=P">P</a></li>
-                    <li><a href="index.php?id=Q">Q</a></li>
-                    <li><a href="index.php?id=R">R</a></li>
-                    <li><a href="index.php?id=S">S</a></li>
-                    <li><a href="index.php?id=T">T</a></li>
-                    <li><a href="index.php?id=U">U</a></li>
-                    <li><a href="index.php?id=V">V</a></li>
-                    <li><a href="index.php?id=W">W</a></li>
-                    <li><a href="index.php?id=X">X</a></li>
-                    <li><a href="index.php?id=Y">Y</a></li>
-                    <li><a href="index.php?id=Z">Z</a></li>
-                    <li><a href="index.php">Favoritos</a></li>
-                </ul>
-            </section>
+
+            <div class="container tabela">
+                <div class="nome-tabela"><p>Nome</p></div>
+                <div class="tel-tabela"><p>Telefone</p></div>
+                <div class="email-tabela"><p>E-mail</p></div>
+                <div class="vazio"></div>
+            </div>
+
+            <?php while($row4 = mysqli_fetch_array($resultadoBusca)){ ?>
             
-            <?php if(isset($_GET['btSerach'])){ ?>
-            <section class="todos-contatos lista-busca">
-                <div class="container titulo titulo-busca">
-                    <h2>Resultado da Pesquisa</h2>
-                </div>
-
-
-                <div class="container tabela">
-                    <div class="nome-tabela"><p>Nome</p></div>
-                    <div class="tel-tabela"><p>Telefone</p></div>
-                    <div class="email-tabela"><p>E-mail</p></div>
-                    <div class="vazio"></div>
-                </div>
-
-                <?php while($row4 = mysqli_fetch_array($resultadoBusca)){ ?>
                 
+            <div class="container registros">
+                <div class="nomes-registros"><p><?php echo ($row4['nome']); ?></p></div>
+                <div class="tel-registros"><p> <?php echo ($row4['tel']); ?></p></div>
+                <div class="email-registros"><p><?php echo ($row4['email']); ?></p></div>
+                <div class="botoes">
+                    <a href="contatoUp.php?id=<?php echo ($row4['idcontatos']) ?>"><i class="fas fa-user-edit"></i></a>
                     
-                <div class="container registros">
-                    <div class="nomes-registros"><p><?php echo ($row4['nome']); ?></p></div>
-                    <div class="tel-registros"><p> <?php echo ($row4['tel']); ?></p></div>
-                    <div class="email-registros"><p><?php echo ($row4['email']); ?></p></div>
-                    <div class="botoes">
-                        <a href="contatoUp.php?id=<?php echo ($row4['idcontatos']) ?>"><i class="fas fa-user-edit"></i></a>
-                        
-                        <?php if($row4['favoritos'] != 0 ){?>
-                        <a href="favoritar.php?id=<?php echo ($row4['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo "favorito-on";?>"></i></a>
-                        <?php }else{ ?>
-                        <a href="favoritar.php?id=<?php echo ($row4['idcontatos']) ?>" class=""><i class="fas fa-star"></i></a>
-                        <?php }?>
+                    <?php if($row4['favoritos'] != 0 ){?>
+                    <a href="favoritar.php?id=<?php echo ($row4['idcontatos']) ?>" class=""><i class="fas fa-star <?php echo "favorito-on";?>"></i></a>
+                    <?php }else{ ?>
+                    <a href="favoritar.php?id=<?php echo ($row4['idcontatos']) ?>" class=""><i class="fas fa-star"></i></a>
+                    <?php }?>
 
-                        <a href="contatoDel.php?id=<?php echo $row4['idcontatos']; ?>"><i class="fas fa-trash"></i></a>
-                    </div>
+                    <a href="contatoDel.php?id=<?php echo $row4['idcontatos']; ?>"><i class="fas fa-trash"></i></a>
                 </div>
-                <?php  }}?>
-            </section>
+            </div>
+            <?php  }}?>
+        </section>
+        
     </main>
    
 </body>
