ef4b37ccaaa4d30448c6085cf8b8ac79de9022f9
EDUARDO OLIVEIRA FERNANDES <eduardo.ofernandes@senacsp.edu.br>
2019-10-01 22:21:12 -0300

    ajustes no disparo de email na agenda3.0 webhost000

--- phpbasico/agenda-nilo/3.0(000webhost)/fale.php
+++ phpbasico/agenda-nilo/3.0(000webhost)/fale.php
@@ -1,49 +1,49 @@
 <?php
-// if(isset($_POST['btGo'])){
+if(isset($_POST['btGo'])){
     
 
-//     $mensagem = "Obrigador!<br>Mensagem enviada com sucesso!<br>Em breve entraremos em contato";
+    $mensagem = "Obrigador!<br>Mensagem enviada com sucesso!<br>Em breve entraremos em contato";
     
 
-//     // verifica qual sistema operacional para ajustar cabeçalho
-//     if(PATH_SEPARATOR == ";"){
-//         // se for windows
-//         $quebraLinha = "\r\n";
+    // verifica qual sistema operacional para ajustar cabeçalho
+    if(PATH_SEPARATOR == ";"){
+        // se for windows
+        $quebraLinha = "\r\n";
 
-//     }else{
-//         // se for outro sistema
-//         $quebraLinha = "\n";
-//     }
+    }else{
+        // se for outro sistema
+        $quebraLinha = "\n";
+    }
 
 
-//     // recebendo os dados do form
-//     $nome = utf8_decode($_POST['txtNome']);
-//     $emailVisitante = utf8_decode($_POST['txtEmail']);
-//     $telefone = utf8_decode($_POST['txtTel']);
+    // recebendo os dados do form
+    $nome = utf8_decode($_POST['txtNome']);
+    $emailVisitante = utf8_decode($_POST['txtEmail']);
+    $telefone = utf8_decode($_POST['txtTel']);
     
-//     $msg = utf8_decode($_POST['txtMsg']);
-//     $destino = "fernandeseduardo93@gmail.com";
+    $msg = utf8_decode($_POST['txtMsg']);
+    $destino = "fernandeseduardo93@gmail.com";
 
-//     $email = "fernandeseduardo93@gmail.com";
-//     $ip =  getenv("REMOTE_ADDR");
+    $email = "fernandeseduardo93@gmail.com";
+    $ip =  getenv("REMOTE_ADDR");
 
-//     // montando msg a ser enviada no corpo do email
-//     $mensagemHtml = 'Nome: '.$nome.'<br> E-mail: '.$emailVisitante.'<br> Telefone: '.$telefone.'<br> Mensagem: '.$msg;
-//     $headers = "MIME-Version: 1.1" . $quebra_linha;
-// 		$headers .= "Content-type: text/html; charset=iso-8859-1" . $quebra_linha;
-// 		$headers .= "From: " . $email . $quebra_linha;
-// 		$headers .= "Cc: " . $emailVisitante . $quebra_linha;
-// 		$headers .= "Reply-To: " . $emailVisitante . $quebra_linha;
-// 		//Note que o email do remetente será usado no campo Reply-To (responder para)
-// 		if ( !mail( $destino, $mensagemHTML, $headers, "-r" . $email ) ) {		//se for postfix
-// 			$headers .= "Return-Path: " .$destino . $quebra_linha;
-// 			//se "não for postfix"
-// 			mail( $destino, $mensagemHTML, $headers );
-//         }
-//         print($mensagem);
-// }
+    // montando msg a ser enviada no corpo do email
+    $mensagemHtml = 'Nome: '.$nome.'<br> E-mail: '.$emailVisitante.'<br> Telefone: '.$telefone.'<br> Mensagem: '.$msg;
+    $headers = "MIME-Version: 1.1" . $quebra_linha;
+    $headers .= "Content-type: text/html; charset=iso-8859-1" . $quebra_linha;
+    $headers .= "From: " . $email . $quebra_linha;
+    $headers .= "Cc: " . $emailVisitante . $quebra_linha;
+    $headers .= "Reply-To: " . $emailVisitante . $quebra_linha;
+    //Note que o email do remetente será usado no campo Reply-To (responder para)
+    if ( !mail( $destino, $mensagemHtml, $headers, "-r" . $email ) ) {		//se for postfix
+        $headers .= "Return-Path: " .$destino . $quebra_linha;
+        //se "não for postfix"
+    $mensagemHtml = 'Nome: '.$nome.'<br> E-mail: '.$emailVisitante.'<br> Telefone: '.$telefone.'<br> Mensagem: '.$msg;
+        mail( $destino, $mensagemHtml, $headers );
+    }
+    print($mensagem);
+}
 
-//NAO ESTA FUNCIONANDO
 
 ?>
 <!DOCTYPE html>
